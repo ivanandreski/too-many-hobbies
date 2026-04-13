5ba9bb4d773ac9ad41acbbe122c8e39a73ab15c5
Ivan Andreski <ivan.andreski@gmail.com>
2026-04-13 20:29:38 +0200

    split index.html to components

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,21 @@ import { GearData } from "./widgets/gear/gear.js"
 
 window.onload = async () => {
   await Promise.all([
+    renderComponent("film"),
+    renderComponent("running"),
+    renderComponent("gear"),
+    renderComponent("cycling"),
+  ]);
+
+  await Promise.all([
     LetterboxdData.init(),
     GearData.init()
   ]);
+}
+
+const renderComponent = async (componentName) => {
+  const componentHtml = await fetchComponent(componentName);
+  const componentEl = document.getElementById(`${componentName}-component`);
+
+  componentEl.innerHTML = componentHtml;
 }
--- frontend/util.js
+++ frontend/util.js
@@ -1,14 +1,22 @@
+const prefix = ENV_PROD
+  ? "/too-many-hobbies"
+  : "";
+
 const fetchJsonData = async (path) => {
   try {
-    const prefix = ENV_PROD
-    ? "/too-many-hobbies"
-    : "";
-    console.log(prefix + path);
-
     const response = await fetch(prefix + path);
     return (await response.json())["data"];
   } catch (e) {
     console.error(e);
     return [];
+  }
+}
+
+const fetchComponent = async (componentName) => {
+  try {
+    const response = await fetch(`/components/${componentName}.html`);
+    return (await response.text());
+  } catch (error) {
+    console.error(error);
   }
 }
