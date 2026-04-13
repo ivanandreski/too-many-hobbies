92405e3bd6a94f6fec3b64b01adf3b09c30ad14a
Ivan Andreski <ivan.andreski@gmail.com>
2026-04-13 19:32:48 +0200

    gear widget

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,9 @@
 import { LetterboxdData } from "./widgets/letterboxd/letterboxd.js";
+import { GearData } from "./widgets/gear/gear.js"
 
 window.onload = async () => {
   await Promise.all([
-    LetterboxdData.init()
+    LetterboxdData.init(),
+    GearData.init()
   ]);
 }
---
+++ frontend/widgets/gear/gear.js
@@ -0,0 +1,22 @@
+const initGear = async () => {
+  const gearData = await parseJson("../../data/gear/bikes.json");
+
+  const mainBikeEntryEl = document.getElementById("main-bike-entry");
+  renderBikeData(mainBikeEntryEl, gearData["mainBike"]);
+
+  const commuterBikeEntry = document.getElementById("commuter-bike-entry");
+  renderBikeData(commuterBikeEntry, gearData["commuter"]);
+}
+
+const renderBikeData = (entryEl, bikeData) => {
+  entryEl.querySelector(".bike-name-title").innerText = bikeData.name;
+  entryEl.querySelector(".bike-milage-title").innerText = bikeData.milage + " KM ridden";
+}
+
+export const GearData = {
+  init: async () => {
+    await Promise.all([
+      initGear()
+    ]);
+  }
+}
